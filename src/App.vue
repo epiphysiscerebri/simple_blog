bad16f4d338b5aa58a932d2ac4eb32959a618651
epiphysiscerebri <zamurievanton@inbox.ru>
2022-10-18 22:55:22 +0300

    create postForm and postList components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-    <div>
+    <div class="app">
+        <PostForm />
+        <PostList :posts="posts"/>
     </div>
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue'
+import PostList from '@/components/PostList.vue'
+
 export default {
+    components: {
+        PostForm,
+        PostList
+    },
     data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Всё о JS'},
+                {id: 2, title: 'Python', body: 'Всё о Python'},
+                {id: 3, title: 'C++' , body: 'Всё о C++'},
+            ],
+            title: '',
+            body: '',
+        }
     },
     methods: {
+        createPost() {
+            const newPost = {
+                id: new Date(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost)
+            this.title = ''
+            this.body = ''
+        },
     }
 }
 </script>
 
 <style>
-
+/* Обнуление стилей браузера по умолчанию */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app {
+    padding: 20px;
+}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <form @submit.prevent>
+        <h4> Создание поста</h4>
+        <input 
+            :value="post.title"
+            @input="post.title = $event.target.value"
+            class="input" 
+            type="text" 
+            placeholder="Название поста">
+        <input 
+            :value="post.body"
+            @input="post.body = $event.target.value"
+            class="input" 
+            type="text" 
+            placeholder="Содержание поста">
+        <button 
+            class="btn"
+        >
+            Создать пост
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            post: {
+                title: '',
+                body: '',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+}
+.input {
+    width: 100%;
+    border: 1px solid purple;
+    padding: 5px 10px;
+    margin-top: 15px;
+}
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: purple;
+    border: 1px solid purple;
+}
+</style>
